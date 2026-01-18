a4dbcad7c5ed986ec0528c32baf8bc03f00de28f
Abhi <abhishek0ghill0000@gmail.com>
2026-01-18 12:35:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const express = require('express');
+const express = require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 10000;
 
 app.get('/', (req, res) => res.send('Alix API working!'));
 
 app.listen(PORT, () => console.log(`Server on port ${PORT}`));
+
